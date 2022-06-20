ac087e5bdb6f4573f97ef19441c3bacbaa971889
Steve C <your@email.com>
2022-06-20 16:44:29 -0400

    ..

---
+++ test/headTest.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert
+const head = require('../head')
+
+describe('#head', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1)
+  })
+})
