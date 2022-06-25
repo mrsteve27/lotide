f4c9c189cc27ec5831c2e5d18a0525333ac35830
Steve C <your@email.com>
2022-06-25 14:57:31 -0400

    cleaned up syntax

--- countOnly.js
+++ countOnly.js
@@ -1,17 +1,17 @@
-const countOnly = function (allItems, itemsToCount) {
-  const results = {}
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
   for (const item of allItems) {
     if (itemsToCount[item]) {
       if (results[item]) {
-        results[item] += 1
+        results[item] += 1;
       } else {
-        results[item] = 1
+        results[item] = 1;
       }
     }
   }
-  return results
-}
-module.exports = countOnly
+  return results;
+};
+module.exports = countOnly;
 
 /*: const firstNames = [
     "Karl",
