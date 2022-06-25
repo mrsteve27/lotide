59dff4c7088399a1ee7c482d50c4683cd14282be
Steve C <your@email.com>
2022-06-25 14:59:18 -0400

    cleaned up syntax/removed test code

--- flatten.js
+++ flatten.js
@@ -1,13 +1,15 @@
-const flatten = function (arr) {
-  let result = []
+const flatten = function(arr) {
+  let result = [];
 
   for (const item of arr) {
     if (Array.isArray(item)) {
-      result = result.concat(flatten(item))
+      result = result.concat(flatten(item));
     } else {
-      result.push(item)
+      result.push(item);
     }
   }
-  return result
-}
-console.log(flatten([1, [2, [3, [4]]]]))
+  return result;
+};
+// console.log(flatten([1, [2, [3, [4]]]]))
+
+module.exports = flatten;
