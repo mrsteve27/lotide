6a585e2fbfe8a750015f81c0df3693f3fda0aa0b
Steve C <your@email.com>
2022-06-25 14:57:05 -0400

    cleaned up syntax

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,16 +1,16 @@
-const eqObjects = require('./eqObjects')
+const eqObjects = require('./eqObjects');
 
-const assertObjectsEqual = function (actual, expected) {
-  const inspect = require('util').inspect
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
 
   if (eqObjects(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)}!== ${inspect(expected)}`)
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)}!== ${inspect(expected)}`);
   } else {
-    console.log(`❌❌❌ Assertion Failed: ${inspect(actual)} === ${inspect(expected)}`)
+    console.log(`❌❌❌ Assertion Failed: ${inspect(actual)} === ${inspect(expected)}`);
   }
-}
+};
 
-module.exports = assertObjectsEqual
+module.exports = assertObjectsEqual;
 /*
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
