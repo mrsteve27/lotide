82f6ea9e9b145f934894c6a342e6b737f739502b
Steve C <your@email.com>
2022-06-25 14:58:47 -0400

    cleaned up syntax

--- findKey.js
+++ findKey.js
@@ -1,21 +1,8 @@
-const findKey = function (object, callback) {
+const findKey = function(object, callback) {
   for (const key in object) {
-    if (callback(object[key])) { return key }
+    if (callback(object[key])) { return key; }
   }
-  return undefined
-}
+  return undefined;
+};
 
-module.exports = findKey
-
-/*
-
-  findKey({
-    "Blue Hill": { stars: 1 },
-    "Akaleri":   { stars: 3 },
-    "noma":      { stars: 2 },
-    "elBulli":   { stars: 3 },
-    "Ora":       { stars: 2 },
-    "Akelarre":  { stars: 3 }
-  }, x => x.stars === 2) // => "noma"
-
-  */
+module.exports = findKey;
