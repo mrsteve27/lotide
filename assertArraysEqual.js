8121fce5761a04369d3fa641c6f96fbe5a73a322
Steve C <your@email.com>
2022-06-25 14:56:46 -0400

    cleaned up syntax

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,12 @@
-const eqArrays = require('./eqArrays')
+const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function (arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`Comparison Passed: ✅✅✅ ${arr1} === ${arr2}`)
-    return
+    console.log(`Comparison Passed: ✅✅✅ ${arr1} === ${arr2}`);
+    return;
   }
 
-  console.log(`Comparison Failed: ❌❌❌ ${arr1} !== ${arr2}`)
-}
+  console.log(`Comparison Failed: ❌❌❌ ${arr1} !== ${arr2}`);
+};
 
-module.exports = assertArraysEqual
+module.exports = assertArraysEqual;
