225436d227f5d295c187fba604bb9cfee0bdcddb
Steve C <your@email.com>
2022-06-25 15:00:55 -0400

    cleaned up syntax

--- letterPositions.js
+++ letterPositions.js
@@ -1,14 +1,14 @@
-const letterPositions = function (sentence) {
-  const results = {}
+const letterPositions = function(sentence) {
+  const results = {};
   for (let i = 0; i < sentence.length; i++) {
     if (sentence[i] !== ' ') {
       if (results[sentence[i]]) {
-        results[sentence[i]].push(i)
+        results[sentence[i]].push(i);
       } else {
-        results[sentence[i]] = [i]
+        results[sentence[i]] = [i];
       }
     }
   }
-  return results
-}
-module.exports = letterPositions
+  return results;
+};
+module.exports = letterPositions;
