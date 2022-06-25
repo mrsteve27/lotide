f3e3099f35b47cc4dc2e3d9f2e0c49e876b1d869
Steve C <your@email.com>
2022-06-25 14:58:28 -0400

    cleaned up syntax

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,14 @@
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    return false
+    return false;
   }
 
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
-module.exports = eqArrays
+module.exports = eqArrays;
