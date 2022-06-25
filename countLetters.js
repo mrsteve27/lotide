8321937ec2e14fb4d946b6f00a6f547e5374e6ea
Steve C <your@email.com>
2022-06-25 14:57:17 -0400

    cleaned up syntax

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,11 @@
-const countLetters = function (string) {
-  const result = {}
+const countLetters = function(string) {
+  const result = {};
 
   for (const letter of string) {
     if (letter !== ' ') {
-      result[letter] = result[letter] + 1 || 1
+      result[letter] = result[letter] + 1 || 1;
     }
   }
-  return result
-}
-module.exports = countLetters
+  return result;
+};
+module.exports = countLetters;
