07ffde709f92b1b8d4f82dede89228bb8f3e8d2c
Steve C <your@email.com>
2022-06-25 15:03:39 -0400

    cleaned up syntax

--- without.js
+++ without.js
@@ -1,11 +1,11 @@
-const without = function (arr1, arr2) {
-  const processedArr = []
+const without = function(arr1, arr2) {
+  const processedArr = [];
   for (const index of arr1) {
     if (!arr2.includes(index)) {
-      processedArr.push(index)
+      processedArr.push(index);
     }
   }
-  return processedArr
-}
+  return processedArr;
+};
 
-module.exports = without
+module.exports = without;
