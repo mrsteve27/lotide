d20626a1259640ee0d0a87ef957da8c9df3960b0
Steve C <your@email.com>
2022-06-25 15:09:48 -0400

    cleaned up syntax

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -11,12 +11,3 @@ const assertObjectsEqual = function(actual, expected) {
 };
 
 module.exports = assertObjectsEqual;
-/*
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-
-assertObjectsEqual(ab,ba);
-assertObjectsEqual(cd,dc);
-    */
